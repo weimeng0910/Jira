1c95a02b2b6b6de8b47c1d7a2074d75c6a71755b
supermeng <76035703+weimeng0910@users.noreply.github.com>
2022-08-02 19:22:30 +0300

    chore commit 02082022

---
+++ src/screens/project-list/index.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
+
+//导入组件
+import SearchPanel from './search-panel';
+import List from './list';
+const apiUrl = process.env.REACT_APP_API_URL;
+export  const ProjectListScreen = () => {
+  //项目名称和项目ID的状态
+  const [param, setParam] = useState({
+    name: '',
+    personId: '',
+  });
+  //定义请求的工程列表的状态
+  const [list, setList] = useState([]);
+
+  //user用户状态
+  const [users, setusers] = useState([]);
+
+  //请求用户数据
+  useEffect(() => {
+    fetch('${apiUrl}/projects').then(async (response) => {
+      if (response.ok) {
+        setList(await response.json());
+      }
+    });
+  }, [param]); //当用户点击下拉，param就会变化触发请求下拉数据
+  useEffect(() => {
+      fetch('${apiUrl}/users').then(async (response) => {
+      if (response.ok) {
+        setusers(await response.json());
+      }
+    });
+
+  }, []);
+  return (
+    <div>
+      <SearchPanel users={users} param={param} setParam={setParam}/>
+      <List list={list} users={users} />
+    </div>
+  );
+};
+
+
---
+++ src/screens/project-list/search-panel.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+
+export default function SearchPanel({param,setParam}) {
+
+
+  //input输入框触发函数
+  function handleChangeInput(evt) {
+    console.log(evt.target.value);
+    //Object.assign()基本用法： Object.assign方法用来将源对象（source）的所有可枚举属性，
+    //复制到目标对象（target）。 它至少需要两个对象作为参数，第一个参数是目标对象，后面的参数都是源对象
+    //  setParam(Object.assign({},param,{name:evt.target.value}))
+    setParam({
+      ...param, //解构对象
+      name: evt.target.value, //重新为对象中的属性赋值
+    });
+  }
+  //handleChangeSelect
+  const handleChangeSelect = (evt) => {
+    setParam({
+      ...param,
+      personId: evt.target.value,
+    });
+  };
+
+  return (
+    <form>
+      <div>
+        <input type='text' value={param.name} onChange={handleChangeInput} />
+        <select value={param.personId} onChange={handleChangeSelect}>
+          <option value={''}>负责人</option>
+          {/* 遍历读取用户列表数据 */}
+          {users.map((user) => {
+            return <option value={user.id}>{user.name}</option>;
+          })}
+        </select>
+      </div>
+    </form>
+  );
+}
