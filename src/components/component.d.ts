fc666ff8a1ee0c37eeff4f3092e65261b1b54f6e
supermeng <76035703+weimeng0910@users.noreply.github.com>
2022-08-18 11:41:24 +0300

    docs

--- src/components/component.d.ts
+++
@@ -1,4 +0,0 @@
-export declare class Component {
-    testMultiplyMethod(num1: number, num2: number): number;
-    hello(): void;
-}
--- src/components/component.ts
+++
@@ -1,9 +0,0 @@
-export class Component {
-  testMultiplyMethod(num1: number, num2: number): number {
-    return num1 * num2;
-  }
-
-  hello(): void {
-    console.log('Hello!');
-  }
-}
