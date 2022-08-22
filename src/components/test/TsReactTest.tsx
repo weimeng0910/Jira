d319ce8ddbab988446e421d63e31945304864a46
supermeng <76035703+weimeng0910@users.noreply.github.com>
2022-08-22 12:28:09 +0300

    test:测试自定义hook

---
+++ src/components/test/TsReactTest.tsx
@@ -0,0 +1,58 @@
+import { Button } from 'antd';
+
+import { useArray } from '@/utils/hooks/useArray';
+import { useMount } from '@/utils/hooks/useMount';
+
+export const TsReactTest = () => {
+    const persons: { name: string; age: number }[] = [
+        {
+            name: 'mengwei',
+            age: 18
+        },
+        {
+            name: 'yulan',
+            age: 16
+        }
+    ];
+
+    const { value, clear, removeIndex, add } = useArray(persons);
+
+    useMount(() => {
+        console.log(value);
+    });
+    return (
+        <div>
+            <div>
+                <h1 style={{ marginLeft: '20px' }}>按钮</h1>
+                <Button
+                    style={{ marginLeft: '20px' }}
+                    type="primary"
+                    onClick={() => add({ name: 'john', age: 22 })}
+                >
+                    add john
+                </Button>
+                <Button
+                    style={{ marginLeft: '20px' }}
+                    type="primary"
+                    onClick={() => removeIndex(0)}
+                >
+                    removeIndex
+                </Button>
+                <Button
+                    style={{ marginLeft: '20px' }}
+                    type="primary"
+                    onClick={() => clear()}
+                >
+                    clear
+                </Button>
+                {value.map((item: { name: string; age: number }, index: number) => (
+                    <div key={index}>
+                        <span>姓名：{item.name}</span>
+                        <br />
+                        <span>年龄：{item.age}</span>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+};
